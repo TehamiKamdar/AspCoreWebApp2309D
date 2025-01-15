e7e4a4780138bf4bc77e6e1b8001ae0c6065ce0a
urbatariq <urbatariq102@gmail.com>
2025-01-15 15:13:53 +0500

    Add project files.

---
+++ Controllers/WebController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspCoreWebApp2309D.Controllers
+{
+    public class WebController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Menu()
+        {
+            return View();
+        }
+    }
+}
